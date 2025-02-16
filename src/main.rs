9485278631fa6c13f2d549bef8d74c2c11c2114a
javohir-abdusattorov <javohirabdusattorov2005@gmail.com>
2025-02-16 16:49:21 +0500

    Responding to PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use core::str;
+use std::{io::{Read, Write}, net::TcpListener};
+
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here! :)");
+    let host = "127.0.0.1:6379";
+    println!("Redis server started at host: {host}");
+    println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+    let listener = TcpListener::bind(host).unwrap();
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                println!("Request: {}", String::from_utf8_lossy(&buf[..]));
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
