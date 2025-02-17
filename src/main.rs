d46e03ed721ecde713fe8b8716020aaff1d6fb0b
javohir-abdusattorov <javohirabdusattorov2005@gmail.com>
2025-02-17 18:09:28 +0500

    Implement handling commands and responding

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,69 @@
 #![allow(unused_imports)]
-use core::str;
-use std::{io::{Read, Write}, net::TcpListener};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use anyhow::Result;
 
+mod resp;
+use resp::*;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let host = "127.0.0.1:6379";
     println!("Redis server started at host: {host}");
-    println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind(host).unwrap();
+    let listener = TcpListener::bind(host).await.unwrap();
+    loop {
+        let stream = listener.accept().await;
 
-    for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                stream.read(&mut buf).unwrap();
-
-                println!("Request: {}", String::from_utf8_lossy(&buf[..]));
-                stream.write(b"+PONG\r\n").unwrap();
+            Ok((stream, addr)) => {
+                println!("connection: {addr:?}");
+                tokio::spawn(async move {
+                    handle_connection(stream).await
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+async fn handle_connection(stream: TcpStream) {
+    let mut handler = RespHandler::new(stream);
+
+    loop {
+        let value = handler.read_value().await.unwrap();
+
+        let response = if let Some(value) = value {
+            println!("command: {value:?}");
+            let (command, args) = extract_command(value).unwrap();
+            match command.to_lowercase().as_str() {
+                "ping" | "command" => Operation::String("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                command => Operation::Error(format!("Unexpected command: {command:?}"))
+            }
+        } else {
+            break;
+        };
+
+        handler.write_value(response).await.unwrap();
+    }
+}
+
+fn extract_command(operation: Operation) -> Result<(String, Vec<Operation>)> {
+    match operation {
+        Operation::Array(vec) => Ok((
+            unpack_bulk(vec.first().unwrap().clone())?,
+            vec.into_iter().skip(1).collect(),
+        )),
+        _ => Err(anyhow::anyhow!("Unexpected command format: {operation:?}")),
+    }
+}
+
+fn unpack_bulk(operation: Operation) -> Result<String> {
+    match operation {
+        Operation::Bulk(str) => Ok(str),
+        _ => Err(anyhow::anyhow!("Unexpected bulk string format: {operation:?}"))
     }
 }
